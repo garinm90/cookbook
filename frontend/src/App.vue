5b99ba8d2ba50cb6ba1d7fdd04c3bc0d20ec3e6a
Max Garin <maxgarin@gmail.com>
2020-12-29 16:45:08 -0500

    Add vue prettier plugin and config

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,8 @@
 <template>
   <div id="app">
     <h1>Recipes</h1>
-    <div
-    v-for="recipe in info"
-    :key="recipe.id"
-    >
-    {{ recipe.recipe_name }}
-
+    <div v-for="recipe in info" :key="recipe.id">
+      {{ recipe.recipe_name }}
     </div>
   </div>
 </template>
@@ -17,16 +13,17 @@ import axios from 'axios'
 export default {
   name: 'App',
 
-  data(){
-    return{
-      info: null
+  data() {
+    return {
+      info: null,
     }
   },
-  mounted(){
-    axios.get('/recipes')
-    .then(response => (this.info = response.data))
-    .catch(error => console.log(error))
-  }
+  mounted() {
+    axios
+      .get('/recipes')
+      .then((response) => (this.info = response.data))
+      .catch((error) => console.log(error))
+  },
 }
 </script>
 
