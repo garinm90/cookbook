7d5d34803412469fe5593a2206a739f57b408e2f
Max Garin <maxgarin@gmail.com>
2020-12-29 16:35:53 -0500

    configure axios baseurl for global access

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'http://localhost:8000'
+
 
 new Vue({
   render: h => h(App),
