b730fdeffc5fb6abccba2738d5b53cabccf985dd
Max Garin <maxgarin@gmail.com>
2020-12-28 21:53:06 -0500

    Add through model for many to many relations to handle quantity mangement of ingredients

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,14 +2,12 @@ from django.contrib import admin
 from .models import (
     Ingredient,
     IngredientQuantity,
-    MeasurementUnit,
+    # IngredientQuantity,
+    # MeasurementUnit,
     Recipe,
-    RecipeIngredients,
 )
 
 
-@admin.register(
-    Ingredient, IngredientQuantity, MeasurementUnit, Recipe, RecipeIngredients
-)
+@admin.register(Ingredient, Recipe, IngredientQuantity)
 class RecipeAdmin(admin.ModelAdmin):
     pass
--- recipes/models.py
+++ recipes/models.py
@@ -7,28 +7,31 @@ class Recipe(models.Model):
     """
 
     recipe_name = models.CharField(max_length=100)
+    ingredient = models.ManyToManyField(
+        "Ingredient", related_name="recipes", through="IngredientQuantity"
+    )
+    print(ingredient)
 
-    pass
-
-
-class RecipeIngredients(models.Model):
-    class Meta:
-        verbose_name = "Recipe Ingredient"
-        verbose_name_plural = "Recipe Ingredients"
-
-    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-
-    pass
+    def __str__(self) -> str:
+        return f"{self.recipe_name}"
 
 
-class MeasurementUnit(models.Model):
+# class RecipeIngredient(models.Model):
+#     class Meta:
+#         verbose_name = "Recipe Ingredient"
+#         verbose_name_plural = "Recipe Ingredients"
+
+#     recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+#     ingredient = models.ManyToManyField("Ingredient", blank=True)
+
+#     def __str__(self) -> str:
+#         return f"{self.recipe_id}"
+
+
+class Ingredient(models.Model):
     """
     docstring
     """
-
-    class Meta:
-        verbose_name = "unit"
-        verbose_name_plural = "units"
 
     VOLUME = "VL"
     MASS = "MS"
@@ -40,22 +43,26 @@ class MeasurementUnit(models.Model):
     UNIT_OF_MEASUREMENT = [
         (OUNCES, "Ounces"),
         (POUNDS, "Pounds"),
-        (GRAMS, "GR"),
+        (GRAMS, "Grams"),
         (FLUID_OUNCES, "Fluid Ounces"),
     ]
 
-    recipe_ingredients_id = models.ForeignKey(
-        RecipeIngredients, on_delete=models.CASCADE
-    )
     mass_or_volume_measurement = models.CharField(
         max_length=2, choices=MASS_OR_VOLUME_MEASUREMENT, default=MASS
     )
+
+    ingredient_name = models.CharField(max_length=100)
     unit_of_measurement = models.CharField(
         max_length=4, choices=UNIT_OF_MEASUREMENT, default=GRAMS
     )
 
+    serving_size = models.IntegerField()
+    fat_per_serving = models.PositiveIntegerField(default=0)
+    carbohydrates_per_serving = models.PositiveIntegerField(default=0)
+    protein_per_serving = models.PositiveIntegerField(default=0)
+
     def __str__(self) -> str:
-        return f"Unit: {self.unit_of_measurement}"
+        return f"{self.ingredient_name}"
 
 
 class IngredientQuantity(models.Model):
@@ -63,28 +70,13 @@ class IngredientQuantity(models.Model):
     docstring
     """
 
-    class Meta:
-        verbose_name = "quantity"
-        verbose_name_plural = "quantities"
-
-    recipe_ingredients_id = models.ForeignKey(
-        RecipeIngredients, on_delete=models.CASCADE
+    ingredient = models.ForeignKey(
+        Ingredient, related_name="quantity", on_delete=models.RESTRICT
     )
-    ingredient_quantity = models.DecimalField(max_digits=12, decimal_places=2)
+    recipe = models.ForeignKey(
+        Recipe, related_name="quantity", on_delete=models.RESTRICT
+    )
+    quantity = models.PositiveIntegerField()
 
     def __str__(self) -> str:
-        return f"Quantity: {self.recipe_quantity}"
-
-
-class Ingredient(models.Model):
-    """
-    docstring
-    """
-
-    recipe_ingredients_id = models.ForeignKey(
-        RecipeIngredients, on_delete=models.CASCADE
-    )
-    ingredient_name = models.CharField(max_length=100)
-
-    def __str__(self) -> str:
-        return f"{self.ingredient_name}"
+        return f"{self.recipe} uses {self.quantity} of {self.ingredient}"
