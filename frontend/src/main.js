cd11b2bd8476c9b41d1349fee6d18cc2f7d7fdf0
Max Garin <maxgarin@gmail.com>
2020-12-29 17:02:11 -0500

    Add vue router and configure  it

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import axios from 'axios'
 
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = 'http://localhost:8000'
 
-
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
