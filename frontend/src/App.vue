72e54a4b82dafbf511eb43cc88806b046adce644
Max Garin <maxgarin@gmail.com>
2020-12-28 21:42:35 -0500

    Install vue.js for frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div id="secret_message">
+      <span v-bind:title="message">
+    Hover your mouse over me for a few seconds
+    to see my dynamically bound title!
+      </span>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  message: message
+}
+ 
+ var message = 'You loaded this page on ' + new Date().toLocaleString()
+  
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
