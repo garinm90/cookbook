2e52bc033d1576e2c3205e291b19c4af66627c98
Max Garin <maxgarin@gmail.com>
2021-01-01 12:43:50 -0500

    Change settings so the app can be properly imported by django

--- cookbook/recipes/apps.py
+++ cookbook/recipes/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class RecipesConfig(AppConfig):
-    name = 'recipes'
+    name = "cookbook.recipes"
+    verbose_name = _("Recipes")
+
+    def ready(self):
+        try:
+            import cookbook.recipes.signals  # noqa F401
+        except ImportError:
+            pass
