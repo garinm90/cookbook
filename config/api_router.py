f4fea467d6059ac8241b2ac81390c841c4d8d6c9
Max Garin <maxgarin@gmail.com>
2021-01-01 12:43:22 -0500

    Add recipes routes to router

--- config/api_router.py
+++ config/api_router.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from cookbook.users.api.views import UserViewSet
+from cookbook.recipes.api.views import IngredientViewSet, RecipeViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -9,6 +10,8 @@ else:
     router = SimpleRouter()
 
 router.register("users", UserViewSet)
+router.register("recipes", RecipeViewSet)
+router.register("ingredients", IngredientViewSet)
 
 
 app_name = "api"
