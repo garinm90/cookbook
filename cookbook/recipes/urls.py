2e70f1d7bb0d5964e865f10e9aa85fd478cb54fc
Max Garin <maxgarin@gmail.com>
2021-01-02 14:17:40 -0500

    Add generic views/urls for recipes and ingredients

--- cookbook/recipes/urls.py
+++ cookbook/recipes/urls.py
@@ -1,4 +1,28 @@
 from django.urls import path
+from .views import (
+    RecipeCreateView,
+    RecipeDetailView,
+    RecipeListView,
+    RecipeUpdateView,
+    IngredientCreateView,
+    IngredientDetailView,
+    IngredientListView,
+    IngredientUpdateView,
+)
+
 
 app_name = "recipes"
-urlpatterns = []
+urlpatterns = [
+    path("", RecipeListView.as_view(), name="recipes"),
+    path("recipe/<int:pk>/", view=RecipeDetailView.as_view(), name="detail"),
+    path("recipe/<int:pk>/update", view=RecipeUpdateView.as_view(), name="update"),
+    path("recipe", view=RecipeCreateView.as_view(), name="create"),
+    path("ingredients", IngredientListView.as_view(), name="ingredients"),
+    path("ingredient/<int:pk>/", view=IngredientDetailView.as_view(), name="detail"),
+    path(
+        "ingredient/<int:pk>/update",
+        view=IngredientUpdateView.as_view(),
+        name="update",
+    ),
+    path("ingredient", view=IngredientCreateView.as_view(), name="create"),
+]
--- cookbook/recipes/views.py
+++ cookbook/recipes/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView, UpdateView, ListView, CreateView
 
-# Create your views here.
+from .models import Ingredient, Recipe
+
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+
+
+class RecipeListView(ListView):
+    model = Recipe
+
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+
+
+class RecipeUpdateView(LoginRequiredMixin, UpdateView):
+    model = Recipe
+
+
+class IngredientDetailView(DetailView):
+    model = Ingredient
+
+
+class IngredientListView(ListView):
+    model = Ingredient
+
+
+class IngredientCreateView(LoginRequiredMixin, CreateView):
+    model = Ingredient
+
+
+class IngredientUpdateView(LoginRequiredMixin, UpdateView):
+    model = Ingredient
