0445888e94a26976d13a00a3450c7a59c47a4cd9
Max Garin <maxgarin@gmail.com>
2021-01-09 15:19:38 -0500

    Add models to admin panel with inline form

--- cookbook/recipes/admin.py
+++ cookbook/recipes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.db import models
+from .models import Ingredient, Recipe, IngredientQuantity
 
-# Register your models here.
+
+class IngredientQuantityInline(admin.TabularInline):
+    model = IngredientQuantity
+    extra = 1
+@admin.register(Ingredient, Recipe)
+class CookbookAdmin(admin.ModelAdmin):
+        inlines = (IngredientQuantityInline,)
